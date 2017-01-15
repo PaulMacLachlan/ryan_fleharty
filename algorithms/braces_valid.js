74e1d0549a6985c96af0946564802ea40c59cca9
ryanfleharty <ryan.fleharty@gmail.com>
2017-01-14 19:11:43 -0600

    Added parens and braces valid algorithms

---
+++ algorithms/braces_valid.js
@@ -0,0 +1,40 @@
+//Given a string and a set of braces, run parens_valid on all of them
+function braces_valid(string){
+    var brace_set = {'(':')', '{':'}', '[':']'}; //the function could be refactored to include brace_set as a parameter instead...
+    var opens = Object.keys(brace_set); //make an array of all the open parentheses
+    var closes = []; //and an array for all the closed parentheses. Object.values would be nice...
+    for ( key in brace_set){
+        closes.push(brace_set[key]);  //populate the array of closed parentheses by adding the value of each key to closes
+        var valence = 0; //give every key an additional value to track the valence of that bracket type
+        brace_set[key] = [brace_set[key]]; //make the values into arrays so we can append the valence to it
+        brace_set[key].push(valence); //now every key has open paren: [close paren, valence count]
+
+    };
+    for(var i=0; i<string.length; i++){ //we finally read through the string
+        var current = string[i]; //this makes later code more readable,  we know current is whatever character we're at in the string
+        if ( opens.indexOf(current) >= 0){ //check if that char is in the opens array
+            brace_set[string[i]][1] += 1; //increase that key's valence value
+            var most_recent_open = current; //this will validate later close parens to make sure you're not mixing types
+        }
+        else if (closes.indexOf(current) >= 0 ){ //check if its in the closes array
+            var location = closes.indexOf(current); //find out which part of the closes array it's at
+            var key = opens[location]; //so we can match the close bracket to the correct open bracket in opens array
+            brace_set[key][1] -=1; //lower that key's valence value
+            if(brace_set[key][1] < 0){
+                return false;
+            }; //whenever we close a parentheses, we want to test- Has the most recent open of a different type been closed?
+            if (key != most_recent_open && brace_set[most_recent_open][1] > 0){; //check if you're mixing types improperly
+                return false;
+            };
+        }; //end of the else procedure for close parens
+    };
+    for( key in brace_set){
+        if(brace_set[key][1] > 0){
+            return false;
+        };
+    };
+    return true;
+};
+
+console.log(braces_valid("({[Hello World]})}"));
+console.log(braces_valid("True True[ () Very {True()}]"));
---
+++ algorithms/parens_valid.js
@@ -0,0 +1,21 @@
+function parens_valid (string){
+    var valence = 0; //valence will count open parentheses as positive and closed as negative. Valence must balance to 0 and never be negative
+    for(var i=0;i<string.length;i++){  //go through the string
+        if (string[i] == '('){ //check if its an open paren
+            valence += 1; //if so, boost the valence counter by one.
+        }
+        else if (string[i] == ')'){ //if its a closed paren, reduce the valence by one
+            valence -= 1;
+        };
+        if (valence < 0){ //after each char is checked, check if there are more closed parens than open
+            return false;
+        };
+    }; //end of the for loop. If code gets past here, valence is either positive or 0. Positive would mean more opens than closes.
+    if (valence != 0){ //If there were more opens than closes, it's still not valid
+        return false;
+    };
+    return true;
+};
+
+console.log(parens_valid("Hello (World)"));
+console.log(parens_valid("(()))))"));
