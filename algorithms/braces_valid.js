80673eeffba4b984cd716170417006b4a1b818a7
ryanfleharty <ryan.fleharty@gmail.com>
2017-01-17 13:03:08 -0600

    Changed the word valence to paren_tracker, despite the poetic elegance of valence

--- algorithms/braces_valid.js
+++ algorithms/braces_valid.js
@@ -8,22 +8,22 @@ function braces_valid(string, brace_set){ //I assume brace_set will be an object
     var unresolved = []; //tracks unresolved open parens in the string
     for ( key in brace_set){ //This loop will revise brace_set to include a tracker for all paren types
         closes.push(brace_set[key]);  //populate the array of closed parentheses by adding the value of each key to closes
-        brace_set[key] = [brace_set[key]]; //make the values into arrays so we can append the valence count to it
-        brace_set[key].push(0); //This adds a 0 valence count to all keys; now every property has open paren: [close paren, valence count]
+        brace_set[key] = [brace_set[key]]; //make the values into arrays so we can append the paren_tracker count to it
+        brace_set[key].push(0); //This adds a 0 paren_tracker count to all keys; now every property has open paren: [close paren, paren_tracker count]
     };
     var current = ""; //this will be used in the for loop to keep track of the current index without nested brackets
     for(var i=0; i<string.length; i++){ //we finally start to read through the string
         current = string[i]; // again, this just makes later code more readable,  not strictly necessary but avoids confusing bracket nesting later
         if (opens.indexOf(current) >= 0){ //check if that char is in the opens array
-            brace_set[current][1] += 1; //increase that key's valence value
+            brace_set[current][1] += 1; //increase that key's paren_tracker value
             unresolved.unshift(current); //add the char to the front of the array of unresolved opens
         }
         else if (closes.indexOf(current) >= 0 ){ //check if its in the closes array
             var location = closes.indexOf(current); //find out which part of the closes array it's at
             var key = opens[location]; //so we can match the close bracket to the correct open bracket using the opens array
-            brace_set[key][1] -=1; //lower that key's valence value
+            brace_set[key][1] -=1; //lower that key's paren_tracker value
             if(brace_set[key][1] < 0){
-                return false; //if valence ever goes negative, it means there have been more closes than opens, which is invalid
+                return false; //if paren_tracker ever goes negative, it means there have been more closes than opens, which is invalid
             }; //whenever we close a parentheses, we want to test- Is there a more recent open of a different type that hasn't closed?
             if (key != unresolved[0]){; //the most recent unresolved paren MUST match the close paren type
                 return false;
@@ -32,7 +32,7 @@ function braces_valid(string, brace_set){ //I assume brace_set will be an object
         }; //end of the else procedure for close parens
     };
     for( key in brace_set){
-        if(brace_set[key][1] > 0){ //if any valence value remains, it means more opens than closes. If it was ever negative we'd have caught that already.
+        if(brace_set[key][1] > 0){ //if any paren_tracker value remains, it means more opens than closes. If it was ever negative we'd have caught that already.
             return false;
         };
     };
--- algorithms/parens_valid.js
+++ algorithms/parens_valid.js
@@ -1,17 +1,17 @@
 function parens_valid (string){
-    var valence = 0; //valence will count open parentheses as positive and closed as negative. Valence must balance to 0 and never be negative
+    var paren_tracker = 0; //paren_tracker will count open parentheses as positive and closed as negative. paren_tracker must balance to 0 and never be negative
     for(var i=0;i<string.length;i++){  //go through the string
         if (string[i] == '('){ //check if its an open paren
-            valence += 1; //if so, boost the valence counter by one.
+            paren_tracker += 1; //if so, boost the paren_tracker counter by one.
         }
-        else if (string[i] == ')'){ //if its a closed paren, reduce the valence by one
-            valence -= 1;
-            if (valence < 0){ //check if there are more closed parens than open
+        else if (string[i] == ')'){ //if its a closed paren, reduce the paren_tracker by one
+            paren_tracker -= 1;
+            if ( paren_tracker < 0){ //check if there are more closed parens than open
                 return false;
             };
         };
-    }; //end of the for loop. If code gets past here, valence is either positive or 0. Positive would mean more opens than closes.
-    if (valence != 0){ //If there were more opens than closes, it's still not valid
+    }; //end of the for loop. If code gets past here, paren_tracker is either positive or 0. Positive would mean more opens than closes.
+    if (paren_tracker != 0){ //If there were more opens than closes, it's still not valid.
         return false;
     };
     return true;
